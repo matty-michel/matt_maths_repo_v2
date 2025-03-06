2cd6634377275ef51d959fb323df67efedd311ab
matty-michel <mmichel@carthage.edu>
2025-03-06 13:52:01 -0600

    updated square function to power function
    now takes in a base & exponent

--- main.cpp
+++ main.cpp
@@ -1,15 +1,25 @@
 #include <iostream>
 using namespace std;
 
-void square(int num) {
-    cout << (num * num) << endl;
+void power(int base, int power) {
+    int result = base;
+    
+    for (int i = 1; i < power; i++) {
+        result = result * base;
+    }
+    
+    cout << result << endl;
 }
 
 //---
 
 int main() {
-    square(4);
-    square(5);
+    
+    power(4, 2);
+    power(5, 3);
+    power(10, 5);
+    power(6, 10);
+    power(10, 8);
 
     return 0;
 }
